4268b7ed06f5f3e2d6d80bc566717745211df00a
Chadwick Maycumber <ChadwickMaycumber@Chads-Mac.local>
2019-02-09 10:20:13 -0500

    Added fonts into site for quicker loads

---
+++ src/utils/typography.js
@@ -0,0 +1,8 @@
+// import Typography from 'typography'
+
+// const typography = new Typography({
+//   headerFontFamily: ['Quattrocento', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica', 'Arial', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'],
+//   bodyFontFamily: ['Nunito Sans', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica', 'Arial', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'],
+// })
+
+// export default typography
